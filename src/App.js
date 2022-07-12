b1a1de93899ccdda14e48eeb9620c1d0a73c6024
EnZooZ <enzooz4889@gmail.com>
2022-07-12 23:45:49 +0700

    Start Asgm3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-class App extends Component {
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
-  render() {
-    return (
-      <div className="App">
-        <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
-        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
-        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
-      </div>
-    );
-  }
+class App extends Component {
+	render() {
+		return (
+			<BrowserRouter>
+				<div>
+					<Main />
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
